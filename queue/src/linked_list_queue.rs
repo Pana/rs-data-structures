c24d8b747d060be0128364bc875497d39d83ff28
pana.wang <become.pan@gmail.com>
2019-09-05 22:10:21 +0800

    add linked list queeue

--- queue/src/linked_list_queue.rs
+++ queue/src/linked_list_queue.rs
@@ -3,7 +3,6 @@ use std::collections::LinkedList;
 
 pub struct LinkedListQueue<T> {
     q: LinkedList<T>,
-    len: usize,
 }
 
 impl<T> LinkedListQueue<T> {
@@ -12,33 +11,26 @@ impl<T> LinkedListQueue<T> {
         let q: LinkedList<T> = LinkedList::new();
         LinkedListQueue{
             q: q,
-            len: 0,
         }
     }
 }
 
 impl<T> Queue<T> for LinkedListQueue<T> {
     fn is_empty(&self) -> bool {
-        self.len == 0
+        self.q.is_empty()
     }
 
     fn len(&self) -> usize {
-        self.len
+        self.q.len()
     }
 
     fn put(&mut self, val: T) -> bool {
-        self.len = self.len + 1;
         self.q.push_back(val);
         true
     }
 
     fn poll(&mut self) -> Option<T> {
-        if self.len == 0 {
-            None
-        } else {
-            self.len -= 1;
-            self.q.pop_front()
-        }
+        self.q.pop_front()
     }
 
     fn print(&self) {
